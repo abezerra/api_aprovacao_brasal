2604cff58101cef61c456be50c3171e84846480f
guaracyalima <guaracyaraujolima@gmail.com>
2018-07-20 17:51:24 -0300

    lv

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -269,8 +269,11 @@ class UserController extends Controller
         // $data['player_id'] = '38da8f63-4f22-49cf-b174-b6c9d70390a6';
         // $data['user_id'] = 5;
         \Log::info($data);
-        return \DB::table("BRASMOB.BRMOB_USUARIOS")->where("USU_IN_CODIGO", '=', $data['user_id'])->update(['player_id' => $data['player_id']]);
+        $save =  \DB::table("BRASMOB.BRMOB_USUARIOS")->where("USU_IN_CODIGO", '=', $data['user_id'])->update(['player_id' => $data['player_id']]);
 
+        \DB::commit();
+
+        return $save;
     }
 
     public function todos_usuarios()
